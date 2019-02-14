2e8b549a25b0b83297a6e98a2813f30b84dee7ed
Freddie White <white.freddie95@gmail.com>
2019-02-14 15:52:04 -0800

    -Worked on features/UX for player tab and page

--- lib/UI/PlayerTab.dart
+++ lib/UI/PlayerTab.dart
@@ -109,225 +109,83 @@ class PlayerPage extends StatelessWidget {
             ),
             height: 150.0,
           ),
+          Container(
+            color: Colors.blue,
+            padding: EdgeInsets.all(10),
+            child: new Text('HOLDING'),
+            height: 50,
+          ),
           GridView.count(
               shrinkWrap: true,
               physics: ClampingScrollPhysics(),
               primary: false,
-              crossAxisSpacing: 2.0,
-              crossAxisCount: 4,
+              padding: EdgeInsets.all(3),
+              crossAxisSpacing: 2,
+              crossAxisCount: 2,
+              childAspectRatio: 2,
               children: <Widget>[
-                Text(titles.name),
-                Text(titles.team),
-                Text(titles.pos),
-                Text(titles.kills),
-                Text(name),
-                Text(team),
-                Text(position),
-                Text(kills),
+                titleTextContainer(titles.name),
+                textContainer(name),
+                titleTextContainer(titles.team),
+                textContainer(team),
+                titleTextContainer(titles.pos),
+                textContainer(position),
+                titleTextContainer(titles.kills),
+                textContainer(kills),
                 //Row two
-                Text(titles.deaths),
-                Text(titles.assists),
-                Text(titles.kda),
-                Text(titles.kp),
-                Text(deaths),
-                Text(assists),
-                Text(kda),
-                Text(kp),
+                titleTextContainer(titles.deaths),
+                textContainer(deaths),
+                titleTextContainer(titles.assists),
+                textContainer(assists),
+                titleTextContainer(titles.kda),
+                textContainer(kda),
+                titleTextContainer(titles.kp),
+                textContainer(kp),
                 //row three
-                Text(titles.cspm),
-                Text(titles.cst),
-                Text(''),
-                Text(''),
-                Text(cspm),
-                Text(csTot),
+                titleTextContainer(titles.cspm),
+                textContainer(cspm),
+                titleTextContainer(titles.cst),
+                textContainer(csTot),
               ]
           ),
         ]));
   }
 }
 
+Widget titleTextContainer(String text){
+  return new Container(
+    child: Center(
+      child: Text(text, style:
+        TextStyle(fontWeight: FontWeight.bold,
+            fontSize: 20.0),),
+    ),
+    decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+            top: BorderSide(width: 1.0, color: Colors.black),
+            bottom: BorderSide(width: 1.0, color: Colors.black),
+            left: BorderSide(width: 1.0, color: Colors.black),
+            right: BorderSide(width: 1.0, color: Colors.black))),
+    margin: EdgeInsets.all(2.0),
+  );
+}
+
+Widget textContainer (String text){
+  return new Container(
+    child: Center(
+      child: Text(text,style: TextStyle(fontSize: 15),),
+    ),
+    decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+            top: BorderSide(width: 1.0, color: Colors.black),
+            bottom: BorderSide(width: 1.0, color: Colors.black),
+            left: BorderSide(width: 1.0, color: Colors.black),
+            right: BorderSide(width: 1.0, color: Colors.black))),
+    margin: EdgeInsets.all(2.0),
+  );
+}
+
 //********************************************************************************************************************************
 //********************************************************************************************************************************
-//Old Player List view Using JSON file
-Widget buildPlayerListView(BuildContext context) {
-  return Scaffold(
-    body: new Container(
-      child: new FutureBuilder(
-          future: DefaultAssetBundle.of(context)
-              .loadString('resources/textFiles/LCS_Summer_2018.json'),
-          builder: (context, snapshot) {
-            var new_data = jsonDecode(snapshot.data.toString());
 
-            //TODO function for score?
-
-            return new ListView.builder(
-                itemBuilder: (BuildContext context, int index) {
-              return new Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border(
-                        bottom: BorderSide(width: 1.0, color: Colors.black),
-                        left: BorderSide(width: 1.0, color: Colors.black),
-                        right: BorderSide(width: 1.0, color: Colors.black))),
-                child: ListTile(
-                  leading: Container(
-                      child: Image.asset(
-                    'resources/images/impact.png',
-                    fit: BoxFit.contain,
-                    height: 90.0,
-                    width: 90.0,
-                  )),
-                  title: new Text((new_data[index]['NAME']).toString()),
-                  subtitle: new Text("Team: " +
-                      (new_data[index]['TEAM']).toString() +
-                      "\tPosition: " +
-                      new_data[index]['POSITION']),
-                  trailing: Text("998"),
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => _buildPlayerPage(
-                                  context,
-                                  playerText(new_data[index]['NAME']),
-                                  Text(new_data[index]['TEAM']),
-                                  Text(new_data[index]['POSITION']),
-                                  playerText((new_data[index]['KDA (RATIO)'])
-                                      .toString()),
-                                  playerText((new_data[index]['KILLS (TOTAL)'])
-                                      .toString()),
-                                  playerText((new_data[index]['DEATHS (TOTAL)'])
-                                      .toString()),
-                                  playerText((new_data[index]
-                                          ['ASSISTS (TOTAL)'])
-                                      .toString()),
-                                  playerText((new_data[index]
-                                          ['KILL PARTICIPATION'])
-                                      .toString()),
-                                  playerText((new_data[index]
-                                          ['CS (PER MINUTE)'])
-                                      .toString()),
-                                  playerText((new_data[index]['CS (TOTAL)'])
-                                      .toString()),
-                                )));
-                  },
-                ),
-              );
-            });
-          }),
-    ),
-  );
-}
-
-//TODO player info
-//Image
-//Stats
-//Build player tile function
-
-Widget _buildPlayerPage(
-    BuildContext context,
-    Widget name,
-    Widget position,
-    Widget team,
-    Widget kda,
-    Widget kills,
-    Widget deaths,
-    Widget assists,
-    var kp,
-    var cspm,
-    var csTot) {
-  return Scaffold(
-    appBar: AppBar(
-      title: name,
-    ),
-    body: ListView(
-      padding: EdgeInsets.all(10.0),
-      children: <Widget>[
-        Container(
-          color: Colors.black,
-          padding: EdgeInsets.symmetric(vertical: 10.0),
-          child: Image.asset(
-            'resources/images/impact.png',
-            fit: BoxFit.contain,
-            height: 150.0,
-          ),
-          height: 150.0,
-        ),
-        //ListView(),
-        //ListView(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(10.0),
-              height: 50.0,
-              width: 100.0,
-              color: Colors.black12,
-              child: Center(
-                child: kda,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(10.0),
-              height: 50.0,
-              width: 100.0,
-              color: Colors.black12,
-              child: Center(
-                child: kills,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(10.0),
-              height: 50.0,
-              width: 100.0,
-              color: Colors.black12,
-              child: Center(
-                child: deaths,
-              ),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(10.0),
-              height: 50.0,
-              width: 100.0,
-              color: Colors.black12,
-              child: Center(
-                child: assists,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(10.0),
-              height: 50.0,
-              width: 100.0,
-              color: Colors.black12,
-              child: Center(
-                child: kp,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(10.0),
-              height: 50.0,
-              width: 100.0,
-              color: Colors.black12,
-              child: Center(
-                child: cspm,
-              ),
-            ),
-          ],
-        ),
-      ],
-    ),
-  );
-}
-
-Text playerText(String data) {
-  return new Text(
-    data,
-    style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-    textAlign: TextAlign.center,
-  );
-}
